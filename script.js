472d6e32ae9d81909361cf2ab8e98d36e3dee05b
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2024-06-25 21:35:23 -0300

    add logo

---
+++ script.js
@@ -0,0 +1,12 @@
+const botaoProjetos = document.querySelector("#projetos");
+const ola = document.querySelector("#ola")
+const carrossel = document.querySelector("#carrossel")
+
+botaoProjetos.addEventListener('click', ()=>{
+    ola.classList.add('esconder')
+    carrossel.classList.remove('esconder')
+    
+    
+} )
+
+
