86d6fa24147acdaa65e4dde8f8c97ba2f324cb39
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2024-06-27 17:27:09 -0300

    desenvolvi footer

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const botaoProjetos = document.querySelector("#projetos");
-const ola = document.querySelector("#ola")
-const carrossel = document.querySelector("#carrossel")
+const ola = document.querySelector("#ola");
+const carrossel = document.querySelector("#carrossel");
+const projetos = document.querySelectorAll(".swiper-slide");
+
+
+
+
 
 botaoProjetos.addEventListener('click', ()=>{
     ola.classList.add('esconder')
-    carrossel.classList.remove('esconder')
+    carrossel.classList.remove('esconder') 
     
     
 } )
+
+
 
 
