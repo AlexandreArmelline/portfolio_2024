e6e9297ca34d965c12a13e29291706f2817c6d5f
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2024-06-28 01:38:03 -0300

    add container expandido

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const botaoProjetos = document.querySelector("#projetos");
 const ola = document.querySelector("#ola");
 const carrossel = document.querySelector("#carrossel");
 const projetos = document.querySelectorAll(".swiper-slide");
-
-
+const containerExpandido = document.querySelector("#containerSwiperExpandidoCulturama")
 
 
 
 botaoProjetos.addEventListener('click', ()=>{
     ola.classList.add('esconder')
     carrossel.classList.remove('esconder') 
-    
-    
+} )
+
+carrossel.addEventListener('click', ()=>{
+    containerExpandido.classList.remove('esconder') 
 } )
 
 
